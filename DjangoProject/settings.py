851727d249843acd1acee3aa7a8cc99d4879b4d0
DarKKnight18812 <artamonovmark3@gmail.com>
2023-01-21 12:57:05 +0300

    FrontEnd и переходы

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5vdy%lqwq0*e-k@*rg5vu)9fj34wnuml(ks9^d&m0#cqb0$$b_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
